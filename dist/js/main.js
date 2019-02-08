e55d373220df8ef546e121907ed114bafa094be4
lucasflaquer <lucas.flaquer@gmail.com>
2019-02-08 18:26:56 -0200

    finalizacao de ajustes

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,5 +1,6 @@
 $(function() {
 	toggleMenu();
+	offsetHeight();
 });
 
 
@@ -12,6 +13,15 @@ function toggleMenu() {
 		$(this).children('.fa').toggleClass('fa-bars');
 		$(this).children('.fa').toggleClass('fa-times');
 
-	});
-	
+	});	
+}
+
+function offsetHeight() {
+	$('a.header--link').click(function(e) {
+		var href = $(this).attr('href');
+		// e.preventDefault();
+		// console.log('href');
+		$("body, html").animate({ 
+    scrollTop: $(href).offset().top - 84   }, "slow");
+	})
 }
